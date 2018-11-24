1faeed167b7e3b0584f866742fac735209c62539
XiNGRZ <chenxingyu92@gmail.com>
2018-11-24 15:39:56 +0800

    fingerprint: goodix: Match input dev name with our KeyHandler
    Change-Id: I1a05f31b133beab0e94b8fe62e183d36ddc5190f

--- drivers/fingerprint/goodix/gf_spi.c
+++ drivers/fingerprint/goodix/gf_spi.c
@@ -62,7 +62,7 @@
 #define GF_SPIDEV_NAME		"goodix,fingerprint"
 /*device name after register in charater*/
 #define GF_DEV_NAME		"goodix_fp"
-#define	GF_INPUT_NAME		"uinput-goodix"
+#define	GF_INPUT_NAME		"gf3206"
 
 #define	CHRD_DRIVER_NAME	"goodix_fp_spi"
 #define	CLASS_NAME		"goodix_fp"
