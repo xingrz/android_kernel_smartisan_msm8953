f327d06d25400b47d8b6f5ac956d904206e0cbfe
XiNGRZ <chenxingyu92@gmail.com>
2018-12-11 16:42:02 +0800

    SMARTISAN: Import changes - hwinfo
    Change-Id: I3376b27773ef501b23d381d51e59adaea04cb8ab

--- drivers/soc/qcom/boot_stats.c
+++ drivers/soc/qcom/boot_stats.c
@@ -30,6 +30,11 @@
 static void __iomem *mpm_counter_base;
 static uint32_t mpm_counter_freq;
 struct boot_stats __iomem *boot_stats;
+#ifdef CONFIG_VENDOR_SMARTISAN
+struct boot_shared_imem_cookie_type __iomem *boot_imem;
+extern u32* log_first_idx_get(void);
+extern u32* log_next_idx_get(void);
+#endif
 
 static int mpm_parse_dt(void)
 {
@@ -121,13 +126,47 @@ unsigned long long int msm_timer_get_sclk_ticks(void)
 	return t1;
 }
 
+#ifdef CONFIG_VENDOR_SMARTISAN
+uint64_t get_boot_reason(void)
+{
+	if (boot_imem == NULL)
+		boot_stats_init();
+	return (uint64_t)boot_imem->pon_reason;
+}
+
+uint32_t get_secure_boot_value(void)
+{
+	if (boot_imem == NULL)
+		boot_stats_init();
+
+	return (uint32_t)boot_imem->is_enable_secure_boot;
+}
+#endif
+
 int boot_stats_init(void)
 {
 	int ret;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	struct device_node *np;
+#endif
 
 	ret = mpm_parse_dt();
 	if (ret < 0)
 		return -ENODEV;
+
+#ifdef CONFIG_VENDOR_SMARTISAN
+	np = of_find_compatible_node(NULL, NULL, "qcom,msm-imem");
+	if (!np) {
+		pr_err("can't find qcom,msm-imem node\n");
+		return -ENODEV;
+	}
+
+	boot_imem = of_iomap(np, 0);
+
+	boot_imem->kernel_log_buf_addr = virt_to_phys(log_buf_addr_get());
+	boot_imem->log_first_idx_addr = (uint32_t)virt_to_phys(log_first_idx_get());
+	boot_imem->log_next_idx_addr = (uint32_t)virt_to_phys(log_next_idx_get());
+#endif
 
 	print_boot_stats();
 
--- include/soc/qcom/boot_stats.h
+++ include/soc/qcom/boot_stats.h
@@ -22,9 +22,64 @@ struct boot_stats {
 	uint32_t bootloader_load_kernel;
 };
 
+#ifdef CONFIG_VENDOR_SMARTISAN
+struct boot_shared_imem_cookie_type
+{
+	/* Magic number which indicates boot shared imem has been initialized
+	   and the content is valid.*/
+	uint32_t shared_imem_magic;
+
+	/* Number to indicate what version of this structure is being used */
+	uint32_t shared_imem_version;
+
+	/* Pointer that points to etb ram dump buffer, should only be set by HLOS */
+	uint64_t etb_buf_addr;
+
+	/* Region where HLOS will write the l2 cache dump buffer start address */
+	uint64_t *l2_cache_dump_buff_ptr;
+
+	/* Magic number for UEFI ram dump, if this cookie is set along with dload magic numbers,
+	   we don't enter dload mode but continue to boot. This cookie should only be set by UEFI*/
+	uint32_t uefi_ram_dump_magic;
+
+	uint32_t ddr_training_cookie;
+
+	/* Abnormal reset cookie used by UEFI */
+	uint32_t abnormal_reset_occurred;
+
+	/* Reset Status Register */
+	uint32_t reset_status_register;
+
+	/* Boot Log Location Pointer to be accessed in UEFI */
+	uint64_t boot_log_addr;
+
+	/* Boot Log Size */
+	uint32_t boot_log_size;
+
+	/*Boot failure count */
+	uint32_t boot_fail_count;
+
+	/*Error code delivery through EDL */
+	uint32_t sbl1_error_type;
+
+	  /* Please add new cookie here, do NOT modify or rearrange the existing cookies*/
+	uint32_t offline_dump_flag;
+	uint64_t kernel_log_buf_addr;
+	uint32_t log_first_idx_addr;
+	uint32_t log_next_idx_addr;
+	uint64_t pon_reason;
+	uint32_t is_enable_secure_boot;
+	uint32_t offline_dump_happen;
+};
+#endif
+
 int boot_stats_init(void);
 int boot_stats_exit(void);
 unsigned long long int msm_timer_get_sclk_ticks(void);
+#ifdef CONFIG_VENDOR_SMARTISAN
+uint64_t get_boot_reason(void);
+uint32_t get_secure_boot_value(void);
+#endif
 #else
 static inline int boot_stats_init(void) { return 0; }
 unsigned long long int msm_timer_get_sclk_ticks(void) { return 0; }
